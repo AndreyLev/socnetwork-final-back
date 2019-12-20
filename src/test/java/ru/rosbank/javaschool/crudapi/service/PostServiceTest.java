b37ac1e8e7d6f5858491b203d6a901b83dee5078
AndreyLev <balashovdon@gmail.com>
2019-12-20 10:05:36 +0300

    removed some things

--- src/test/java/ru/rosbank/javaschool/crudapi/service/PostServiceTest.java
+++ src/test/java/ru/rosbank/javaschool/crudapi/service/PostServiceTest.java
@@ -3,27 +3,20 @@ package ru.rosbank.javaschool.crudapi.service;
 import lombok.val;
 import org.junit.jupiter.api.Test;
 import org.mapstruct.factory.Mappers;
-import ru.rosbank.javaschool.crudapi.dto.UserSaveRequestDto;
 import ru.rosbank.javaschool.crudapi.entity.PostEntity;
 import ru.rosbank.javaschool.crudapi.entity.UserEntity;
 import ru.rosbank.javaschool.crudapi.exception.NotFoundException;
 import ru.rosbank.javaschool.crudapi.mapper.PostMapper;
-import ru.rosbank.javaschool.crudapi.mapper.UserMapper;
 import ru.rosbank.javaschool.crudapi.repository.PostRepository;
-import ru.rosbank.javaschool.crudapi.repository.UserRepository;
 
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.*;
 
 class PostServiceTest {
-
-    // LASTS FIVE POSTS TESTS
 
     @Test
     void getZeroValueLastFivePostsWhereNoPostsInRepo() {
@@ -45,8 +38,6 @@ class PostServiceTest {
 
         assertEquals(3, service.getLastFivePosts().size());
     }
-
-    // TEST FOR NEW POSTS QUANTITY
 
     @Test
     void getNewPostsQuantityWhenPostsPresentInRepo() {
@@ -80,7 +71,6 @@ class PostServiceTest {
         assertEquals(0, service.getNewPostsQuantity(4));
     }
 
-    // TESTS BEFORE POSTS
     @Test
     void getNoRemovePostBeforeFirstDrawnPostWhereNoPostsInRepo() {
         val repository = mock(PostRepository.class);
@@ -114,7 +104,6 @@ class PostServiceTest {
     }
 
 
-    // TESTS AFTER POSTS
     @Test
     void getNoRemovePostAfterLastDrawnPostWhereNoPostsInRepo() {
         val repository = mock(PostRepository.class);
@@ -148,7 +137,6 @@ class PostServiceTest {
         assertEquals(0, service.getNoRemovePostBeforeFirstDrawnPost(4).size());
     }
 
-    // TESTS FOR GET ALL NO REMOVED
 
     @Test
     void getAllNoRemovedWhenRemovedPostsPresentInRepo() {
@@ -183,8 +171,6 @@ class PostServiceTest {
         assertEquals(3, service.getAllNoRemoved().size());
     }
 
-    // TESTS FOR GET DRAWN POSTS
-
     @Test
     void getDrawnPostsWhenPostsPresentInRepo() {
         val repository = mock(PostRepository.class);
@@ -208,9 +194,6 @@ class PostServiceTest {
 
 
 
-
-    // TESTS TO FIX
-
     @Test
     void getByIdShouldThrowExceptionWhereNoPostsInRepo() {
         val repository = mock(PostRepository.class);
@@ -230,19 +213,4 @@ class PostServiceTest {
         assertThrows(NotFoundException.class, () -> service.getById(2));
     }
 
-    // TODO : FIX
-//    @Test
-//    void getByIdShouldReturnDtoWhenPostPresentInRepo() {
-//        val repository = mock(PostRepository.class);
-//        val userRepository = mock(UserRepository.class);
-//        val userService = new UserService(userRepository, Mappers.getMapper(UserMapper.class));
-//         doReturn(Optional.of(new PostEntity(0, userService.save(new UserSaveRequestDto(0, "Случайный прохожий", "anonymous", "11111111", "anonymous@gmail.com"))
-//                 ,"First", null, false, 0)))
-//         .when(repository).findById(1);
-//
-//
-//        val service = new PostService(repository, Mappers.getMapper(PostMapper.class));
-//
-//        assertNotNull(service.getById(1));
-//    }
 }
--- src/test/java/ru/rosbank/javaschool/crudapi/service/UserServiceTest.java
+++ src/test/java/ru/rosbank/javaschool/crudapi/service/UserServiceTest.java
@@ -3,20 +3,14 @@ package ru.rosbank.javaschool.crudapi.service;
 import lombok.val;
 import org.junit.jupiter.api.Test;
 import org.mapstruct.factory.Mappers;
-import ru.rosbank.javaschool.crudapi.dto.UserResponseDto;
-import ru.rosbank.javaschool.crudapi.entity.PostEntity;
 import ru.rosbank.javaschool.crudapi.entity.UserEntity;
 import ru.rosbank.javaschool.crudapi.exception.NotFoundException;
-import ru.rosbank.javaschool.crudapi.mapper.PostMapper;
 import ru.rosbank.javaschool.crudapi.mapper.UserMapper;
-import ru.rosbank.javaschool.crudapi.repository.PostRepository;
 import ru.rosbank.javaschool.crudapi.repository.UserRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 
